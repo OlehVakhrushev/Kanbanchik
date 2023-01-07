188c81bdc33b5120eda76977339b1f0e32fa75e0
Akradabas <akradabaska@gmail.com>
2023-01-07 15:01:35 -0800

    initial PORT commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const routes = require("./routes");
 const bodyParser = require("./bodyParser");
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 routes(app);
 bodyParser(app);
